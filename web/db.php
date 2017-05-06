de19110d53b5986cf43573a4f4f7a12ad0c7f005
Isaac <yiesheng.chua@gmail.com>
2017-05-07 03:42:49 +1000

    Add wrapper functions for table fields

--- web/db.php
+++ web/db.php
@@ -1,8 +1,11 @@
 <?php
-  $dir = "sqlite:../db/home.db";
-  $pdo = new PDO($dir);
+  $pdo = new PDO("sqlite:../db/home.db");
   $stmt = $pdo->query("SELECT * FROM heater WHERE heater_id = 1");
   $row = $stmt->fetchObject();
+
+  function GetUpdateSql($field, $val) {
+    return sprintf("UPDATE heater SET %s = %d WHERE heater_id = 1", $field, $val);
+  }
 
   function GetCurrentPower() {
     global $row;
@@ -10,12 +13,55 @@
   }
 
   function SetCurrentPower($power) {
-    global $pdo;
-    $stmt = $pdo->prepare(GetUpdateSql("current_power", $power));
-    $stmt->execute();
+    if ($power != GetCurrentPower()) {
+      global $pdo;
+      $stmt = $pdo->prepare(GetUpdateSql("current_power", $power));
+      $stmt->execute();
+    }
   }
 
-  function GetUpdateSql($field, $val) {
-    return sprintf("UPDATE heater set %s = %d WHERE heater_id = 1", $field, $val);
+  function GetSelectedPower() {
+    global $row;
+    return ($row->selected_power == 1);
+  }
+
+  function SetSelectedPower($power) {
+    if ($power != GetSelectedPower()) {
+      global $pdo;
+      $stmt = $pdo->prepare(GetUpdateSql("selected_power", $power));
+      $stmt->execute();
+    }
+  }
+
+  function BoundTemperature($temperature) {
+    $max = 20.0;
+    $min = 10.0;
+    return min($max, max($min, $temperature));
+  }
+
+  function GetCurrentTemperature() {
+    global $row;
+    return $row->current_temperature;
+  }
+
+  function SetCurrentTemperature($temperature) {
+    if ($temperature != GetCurrentTemperature()) {
+      global $pdo;
+      $stmt = $pdo->prepare(GetUpdateSql("current_temperature", BoundTemperature($temperature)));
+      $stmt->execute();
+    }
+  }
+
+  function GetSelectedTemperature() {
+    global $row;
+    return $row->selected_temperature;
+  }
+
+  function SetSelectedTemperature($temperature) {
+    if ($temperature != GetSelectedTemperature()) {
+      global $pdo;
+      $stmt = $pdo->prepare(GetUpdateSql("selected_temperature", BoundTemperature($temperature)));
+      $stmt->execute();
+    }
   }
 ?>
