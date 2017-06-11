dabda8dbd631a5f2118683567d63c82ecee29999
Isaac <yiesheng.chua@gmail.com>
2017-06-11 23:50:03 +1000

    Update schema
    Add timeout datetime field

--- db/home.sql
+++ db/home.sql
@@ -6,7 +6,8 @@ CREATE TABLE "heater" (
 	`current_power`	NUMERIC NOT NULL DEFAULT 0,
 	`current_temperature`	REAL NOT NULL DEFAULT 10.0,
 	`is_active`	NUMERIC NOT NULL DEFAULT 0,
+	`timeout`	TEXT,
 	PRIMARY KEY(heater_id)
 );
-INSERT INTO `heater` (heater_id,selected_power,selected_temperature,current_power,current_temperature,is_active) VALUES (1,0,17.0,0,17.0,0);
+INSERT INTO `heater` (heater_id,selected_power,selected_temperature,current_power,current_temperature,is_active,timeout) VALUES (1,0,10.0,0,10.0,0,NULL);
 COMMIT;
