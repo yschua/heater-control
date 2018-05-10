ca30738af9e428b6941e21b78b51fe3fa2873f78
Isaac <yiesheng.chua@gmail.com>
2018-05-10 23:51:28 +1000

    Add default timeout on power on

--- web/updatedb.php
+++ web/updatedb.php
@@ -1,6 +1,10 @@
 <?php
   include "db.php";
 
+  const DEFAULT_TIMEOUT = 30;
+  const MIN_TEMP = 10;
+  const MAX_TEMP = 20;
+
   $message = $_POST["message"];
   if (isset($message)) {
     $message = substr($message, 1);
@@ -11,17 +15,19 @@
     if (strtolower($operation) == "p") {
       if ($value == 0 || $value == 1) {
         SetSelectedPower($value);
-        if ($operation == "P") {
+        if ($operation == "P") { // calibrate message
           SetCurrentPower($value);
         }
-        if ($value == 0) {
+        if ($value == 0) { // on power off
           SetTimeout(0);
+        } else if (GetSelectedPower() != 1) { // always default to a timeout value
+          SetTimeout(DEFAULT_TIMEOUT);
         }
       }
     } else if (strtolower($operation) == "t") {
-      if ($value >= 10 && $value <= 20) {
+      if ($value >= MIN_TEMP && $value <= MAX_TEMP) {
         SetSelectedTemperature($value);
-        if ($operation == "T") {
+        if ($operation == "T") { // calibrate message
           SetCurrentTemperature($value);
         }
       }
